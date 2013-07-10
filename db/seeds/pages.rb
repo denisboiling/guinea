7a3a452d8c7f8e761539b4ecad02b1ad72feba9c
novoselov <novsem@gmail.com>
2013-07-10 10:14:02 +0300

    makeup: add footer

--- db/seeds/pages.rb
+++ db/seeds/pages.rb
@@ -1,5 +1,16 @@
 #coding: utf-8
 
+puts "locale: #{I18n.locale}"
+
 Page.reset_column_information
-FactoryGirl.create(:page, slug: 'index')
-FactoryGirl.create(:page, slug: 'contacts')
+Page.create!(slug: 'index', name: 'Главная', hidden: true)
+Page.create!(slug: 'about', name: 'О нас')
+Page.create!(slug: 'info_for_borrower', name: 'Информация для заемщика')
+Page.create!(slug: 'current_lenging_programs',
+             name: 'Действующие программы предоставления займов')
+Page.create!(slug: 'insurance', name: 'Страхование')
+Page.create!(slug: 'your_credit_history', name: 'Ваша кредитная история', hidden: true)
+Page.create!(slug: 'correct_credit_history', name: 'Исправить кредитную историю', hidden: true)
+Page.create!(slug: 'fill_online_request', name: 'Заполнить заявку Online', hidden: true)
+Page.create!(slug: 'contact_us', name: 'Связаться с нами', hidden: true)
+Page.create!(slug: 'inbki_reference', name: 'Справка ИНБКИ', hidden: true)
