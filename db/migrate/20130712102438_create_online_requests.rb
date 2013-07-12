858aba14d0dba289b166e7e1ec683b16aeced097
novoselov <novsem@gmail.com>
2013-07-12 15:23:46 +0300

    online-requests: add model and specs

---
+++ db/migrate/20130712102438_create_online_requests.rb
@@ -0,0 +1,15 @@
+class CreateOnlineRequests < ActiveRecord::Migration
+  def change
+    create_table :online_requests do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :patronymic
+      t.string :phone
+      t.string :email
+      t.string :registration_address
+      t.datetime :birth_date
+
+      t.timestamps
+    end
+  end
+end
